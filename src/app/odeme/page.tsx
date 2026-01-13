eca91ea8e1ae6fd632fef7a5624dea30acb829bd
berkaykrks <ata.karakus@std.medipol.edu.tr>
2026-01-13 21:55:44 +0300

    rota ttt

--- src/app/odeme/page.tsx
+++ src/app/odeme/page.tsx
@@ -16,7 +16,7 @@ export default function OdemePage() {
      * Test için: https://entegrasyon.asseco-see.com.tr/fim/est3Dgate
      * Canlı için: https://sanalpos2.ziraatbank.com.tr/fim/est3Dgate
      */
-    form.action = "https://sanalpos2.ziraatbank.com.tr/fim/est3Dgate";
+    form.action = "https://entegrasyon.asseco-see.com.tr/fim/est3Dgate";
 
     Object.keys(data).forEach((key) => {
       const input = document.createElement("input");
